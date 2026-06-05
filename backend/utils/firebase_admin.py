7b673ee6c95e71666a02e82256030619add04a50
D-moc <karmadinesh159@gmail.com>
2026-06-05 17:58:20 +0530

    firebase render setup

--- backend/utils/firebase_admin.py
+++ backend/utils/firebase_admin.py
@@ -1,10 +1,22 @@
-from firebase_admin import credentials, auth
+import os
+import json
+
+from firebase_admin import credentials
+from firebase_admin import auth
 import firebase_admin
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_credentials = json.loads(
+    os.environ["FIREBASE_CREDENTIALS"]
+)
+
+cred = credentials.Certificate(
+    firebase_credentials
+)
 
 if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
+    firebase_admin.initialize_app(
+        cred
+    )
 
 def verify_firebase_token(id_token: str):
     try:
